04c3d5d3f0d25225ad6e06529991997bdab555cf
Pawel Wujczyk <pwujczyk@hotmail.com>
2017-12-04 20:09:02 +0100

    initial commit

---
+++ Copy-ItemDirectoryRepeatable/Copy-ItemDirectoryRepeatable.psm1
@@ -0,0 +1,21 @@
+function Copy-ItemDirectoryRepeatable {
+
+	[cmdletbinding()]
+	param ([string]$LiteralPath, [string]$Destination, [switch]$Force, [Switch]$Recurse)
+	
+	New-Item -Path $Destination -Force -ItemType Directory
+	if ($Recurse.IsPresent)
+	{
+		$sourceItems=Get-ChildItem -Path $LiteralPath
+	}
+	else {
+		$sourceItems=Get-ChildItem -Path $LiteralPath -File
+	}
+	foreach($item in $sourceItems)
+	{
+		$source=$item.FullName
+		Get-ChildItem $source
+		Copy-Item -LiteralPath $source -Destination $Destination -Force:$Force -Recurse:$Recurse -Verbose
+	}
+
+}
