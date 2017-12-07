b5db13600b1eda52b376426d00764e48a4311537
Pawel Wujczyk <pwujczyk@hotmail.com>
2017-12-07 19:32:37 +0100

    do not display new created directory

--- Copy-ItemDirectoryRepeatable/Copy-ItemDirectoryRepeatable.psm1
+++ Copy-ItemDirectoryRepeatable/Copy-ItemDirectoryRepeatable.psm1
@@ -3,7 +3,7 @@ function Copy-ItemDirectoryRepeatable {
 	[cmdletbinding()]
 	param ([string]$LiteralPath, [string]$Destination, [switch]$Force, [Switch]$Recurse)
 	
-	New-Item -Path $Destination -Force -ItemType Directory
+	New-Item -Path $Destination -Force -ItemType Directory | Out-Null
 	if ($Recurse.IsPresent)
 	{
 		$sourceItems=Get-ChildItem -Path $LiteralPath
@@ -14,8 +14,8 @@ function Copy-ItemDirectoryRepeatable {
 	foreach($item in $sourceItems)
 	{
 		$source=$item.FullName
-		Get-ChildItem $source
-		Copy-Item -LiteralPath $source -Destination $Destination -Force:$Force -Recurse:$Recurse -Verbose
+		#Get-ChildItem $source
+		Copy-Item -LiteralPath $source -Destination $Destination -Force:$Force -Recurse:$Recurse -Verbose:$VerbosePreference 
 	}
 
 }
